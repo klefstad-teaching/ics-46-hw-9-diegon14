148b07a816fce38d2b5542ed45bbfac28318297c
Diego Nunez <diegon1@uci.edu>
2025-03-10 07:32:45 +0000

    changed print_path method

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -49,5 +49,5 @@ void print_path(const vector<int>& v, int total) {
         cout << v[i] << " ";
     }
     cout << endl;
-    cout << "Total: " << total << endl;
+    cout << "Total cost is " << total << endl;
 }
