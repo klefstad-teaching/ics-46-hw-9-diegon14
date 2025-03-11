1d13c027f9e0b6a1e06c5622626d09912b0a3d36
Diego Nunez <diegon1@uci.edu>
2025-03-11 18:10:33 +0000

    Refactored Edit_distance by recursive or conditional

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -24,9 +24,18 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
     if (str1[len1 - 1] == str2[len2 - 1]) {
         return edit_distance_within(str1.substr(0, len1 - 1), str2.substr(0, len2 - 1), d);
     } else {
-        return edit_distance_within(str1, str2.substr(0, len2 - 1), d - 1) ||
-               edit_distance_within(str1.substr(0, len1 - 1), str2, d - 1) ||
-               edit_distance_within(str1.substr(0, len1 - 1), str2.substr(0, len2 - 1), d - 1);
+        if (edit_distance_within(str1, str2.substr(0, len2 - 1), d - 1)) {
+            return true;
+        }
+
+        if (edit_distance_within(str1.substr(0, len1 - 1), str2, d - 1)) {
+            return true;
+        }
+        
+        if (edit_distance_within(str1.substr(0, len1 - 1), str2.substr(0, len2 - 1), d - 1)) {
+            return true;
+        }
+        return false;
     }
 }
 
